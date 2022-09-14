74fca7d20b707a99bdce8a7f835ddee9c16e1ea3
KristianPalkovic <Kristian.Palkovic@sosholic.sk>
2022-09-14 11:06:55 +0200

    Initial commit

---
+++ src/main/java/sk/sosholic/prvypolrok/Main.java
@@ -0,0 +1,8 @@
+package sksosholic.PrvyPolrok;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("veta1");
+        System.out.println("veta2");
+    }
+}
