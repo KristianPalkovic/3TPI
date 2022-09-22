1c4d51bd518f3b8c803e4be19566a1cf86fd7f86
KristianPalkovic <Kristian.Palkovic@sosholic.sk>
2022-09-22 11:02:32 +0200

    commit

---
+++ src/main/java/sk/sosholic/prvypolrok/BasicScanner.java
@@ -0,0 +1,15 @@
+package sk.sosholic.prvypolrok;
+import java.util.Scanner;
+
+public class BasicScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int exit = 1;
+        while(exit!=0){
+            System.out.println("ukoncenie programu stlac 0 a enter");
+            exit = scanner.nextInt();
+            String out = scanner.nextLine();
+
+        }
+    }
+}
--- src/main/java/sk/sosholic/prvypolrok/opakovanie/SystemIn.java
+++ src/main/java/sk/sosholic/prvypolrok/opakovanie/SystemIn.java
@@ -6,7 +6,29 @@ public class SystemIn {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("zadaj volaco a stlac enter: ");
+        /*System.out.println("zadaj volaco a stlac enter: ");
         System.out.println(scanner.nextLine());
+
+        String ulozenyRiadok = scanner.nextLine();
+        int ulozenyRiadok1= scanner.nextInt();
+            scanner.next();
+            scanner.nextBoolean();
+        System.out.println(ulozenyRiadok);*/
+
+
+        int exit = 1;
+        while(exit!=0) {
+
+            System.out.println("Toto je moj prvy program s nazvom Kalkulacka. ");
+            System.out.println("Tento program scitava dve zadane cisla");
+            System.out.print("Zadaj prve cislo a stlac enter: ");
+            int scitanec1 = scanner.nextInt();
+            System.out.print("Zadaj druhe cislo a stlac enter:");
+            int scitanec2 = scanner.nextInt();
+            System.out.println("Vysledok scitania je: " + (scitanec1 + scitanec2));
+            System.out.println("Ukoncenie programu stlac 0 a enter. Pre pokracovanie stlac 1");
+            exit = scanner.nextInt();
+            String out = scanner.nextLine();
+        }
     }
 }
