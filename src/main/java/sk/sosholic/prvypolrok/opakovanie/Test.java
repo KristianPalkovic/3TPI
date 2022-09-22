abb148a9bd30e04d1a88bde8a6903be6600055bb
KristianPalkovic <Kristian.Palkovic@sosholic.sk>
2022-09-22 08:58:37 +0200

    commit

--- src/main/java/sk/sosholic/prvypolrok/opakovanie/Test.java
+++ src/main/java/sk/sosholic/prvypolrok/opakovanie/Test.java
@@ -1,6 +1,9 @@
 package sk.sosholic.prvypolrok.opakovanie;
+
 import java.util.Scanner;
-public class CusNe {
+
+public class Test {
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -11,12 +14,12 @@ public class CusNe {
 
         //dopln zbytok tak aby program vyzadoval zadanie mena a priezviska od konzoly
 
-        MojeMeno mojeMeno = new MojeMeno(meno, priezvisko);
+        MojeMeno mojeMeno = new MojeMeno(meno, "Palkovic");
 
         for (int x = 1; x <=10 ; x++){
             //System.out.println("Kristian Palkovic" + x);
             if(x%2==0)
-            System.out.println(mojeMeno.getCeleMeno() + " " +x);
+                System.out.println(mojeMeno.getCeleMeno() + " " +x);
         }
     }
 }
