d4fa7f858f1565fa193e2187320e7063d7882aa2
KristianPalkovic <Kristian.Palkovic@sosholic.sk>
2022-12-08 08:33:56 +0100

    commit

--- src/main/java/sk/sosholic/prvypolrok/uvod/Main.java
+++ src/main/java/sk/sosholic/prvypolrok/uvod/Main.java
@@ -2,7 +2,6 @@ package sk.sosholic.prvypolrok.uvod;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Moj prvy skompilovany program :) ");
     }
 }
