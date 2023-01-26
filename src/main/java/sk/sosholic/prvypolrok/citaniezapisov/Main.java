f935f5009cb1096891caa96fa36b7d0309e2562f
KristianPalkovic <Kristian.Palkovic@sosholic.sk>
2023-01-26 11:10:08 +0100

    commit

--- src/main/java/sk/sosholic/prvypolrok/citaniezapisov/Main.java
+++ src/main/java/sk/sosholic/prvypolrok/citaniezapisov/Main.java
@@ -1,15 +1,16 @@
 package sk.sosholic.prvypolrok.citaniezapisov;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) {
-        /*FileWriter file = new FileWriter("nejakySubor.txt");
+    public static void main(String[] args) throws IOException {
+        FileWriter file = new FileWriter("nejakySubor.txt");
         file.write("moje meno je " + System.getProperty("user.name"));
         file.close();
 
-        BufferedWriter writer = new BufferedWritter(new FileWriter("test.txt"));
+        /*BufferedWriter writer = new BufferedWritter(new FileWriter("test.txt"));
 
          */
     }
